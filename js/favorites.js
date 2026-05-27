8e9b35b23c4aaef43024517440d14be9d3f1c744
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-05-27 14:58:29 +0200

    implement favorites functionality with localStorage

--- js/favorites.js
+++ js/favorites.js
@@ -1,14 +1,33 @@
 // favorites.js
 
-let favorites = [];
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+
+// ADD FAVORITE
 export function addFavorite(book) {
+
   favorites.push(book);
-  console.log("Added to favorites:", book);
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+
+  console.log("Added:", book);
 }
+
+
+// REMOVE FAVORITE
 export function removeFavorite(bookTitle) {
-  favorites = favorites.filter(book => book.title !== bookTitle);
+
+  favorites = favorites.filter(
+    (book) => book.title !== bookTitle
+  );
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+
   console.log("Removed:", bookTitle);
 }
+
+
+// GET FAVORITES
 export function getFavorites() {
   return favorites;
 }
